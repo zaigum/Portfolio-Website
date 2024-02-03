4a03761eba68d4be457ba8bb77aab45118440089
zaigum <143170220+zaigum@users.noreply.github.com>
2024-02-03 23:52:14 +0500

    Footer complete

--- client/src/PortfolioContainer/MainFooter/Main-Footer.js
+++ client/src/PortfolioContainer/MainFooter/Main-Footer.js
@@ -5,19 +5,30 @@ function Footer() {
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        <p>© 2024 Your Website. All rights reserved.</p>
-         
-        {/* Additional icons */}
-        <div className="colz-icon">
-          <a href="#"><i className="fa fa-facebook-square"></i></a>
-          <a href="#"><i className="fa fa-google-plus-square"></i></a>
-          <a href="#"><i className="fa fa-instagram"></i></a>
-          <a href="#"><i className="fa fa-youtube-square"></i></a>
-          <a href="#"><i className="fa fa-twitter"></i></a>
+        <div className="footer-logo"></div>
+        <div className="footer-middle">
+          <h2>Contact Us</h2>  
+          <ul>
+            <li><i className="fa fa-envelope"></i> Email: example@example.com</li>
+            <li><i className="fa fa-phone"></i> Phone: +1234567890</li>
+            <li><i className="fa fa-map-marker"></i> Address: 123 Street, City, Country</li>
+          </ul>
+        </div>
+        <div className="footer-icons">
+          <h2>Social Media</h2>  
+          <div className="custom-icon">
+            <a href="#"><i className="fa fa-facebook-square"></i></a>
+            <a href="#"><i className="fa fa-google-plus-square"></i></a>
+            <a href="#"><i className="fa fa-instagram"></i></a>
+            <a href="#"><i className="fa fa-youtube-square"></i></a>
+            <a href="#"><i className="fa fa-twitter"></i></a>
+          </div>
         </div>
       </div>
+      <p className="additional-content">© 2024 Your Website. All rights reserved.</p>
     </footer>
   );
 }
 
 export default Footer;
+ 
